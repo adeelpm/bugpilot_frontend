dfc2ad175ad0233af8ce295c8ab6c100de097ab4
adeelpm <abdurrahmanadeel@gmail.com>
2021-06-18 23:00:41 +0530

    update build

--- src/util/headers.js
+++ src/util/headers.js
@@ -2,7 +2,7 @@ import Cookies from 'universal-cookie'
 const cookies = new Cookies()
 const headers={
     headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        
         authorization: cookies.get('token'),
     }
 }
